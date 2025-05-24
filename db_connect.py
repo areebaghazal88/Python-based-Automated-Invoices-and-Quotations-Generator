2d45ce330092cae2446f7a4fcec3512bae3e0fbc
Areeba Ghazal <areebaghazal88@gmail.com>
2025-05-24 21:15:45 +0500

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -4,7 +4,7 @@ def connect_db():
     return pymysql.connect(
         host='localhost',
         user='root',
-        password='areeba_126356',
+        password='your_password123',
         database='invoice_system'
     )
 
